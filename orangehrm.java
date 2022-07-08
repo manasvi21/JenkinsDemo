181a315cf4caf3eeabf483c92d177a210593ad45
manasvi21 <99867384+manasvi21@users.noreply.github.com>
2022-07-08 12:58:10 +0530

    Create orangehrm.java

---
+++ orangehrm.java
@@ -0,0 +1,20 @@
+package orangehrm;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LogIn {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@name='txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@name='txtPassword']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//input[@id='btnLogin']")).click();		
+	}
+
+}
